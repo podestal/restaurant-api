c069a7024f7e6a0c2075d0d936069fc20525a79c
podestal <luisrodriguezpodesta@gmail.com>
2024-12-17 11:23:34 -0500

    web socket update for production

--- app/restaurant/asgi.py
+++ app/restaurant/asgi.py
@@ -1,15 +1,16 @@
-# asgi.py
 import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-from api.routing import websocket_urlpatterns
+import api.routing  # Import your routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'restaurant.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     "websocket": AuthMiddlewareStack(
-        URLRouter(websocket_urlpatterns)
+        URLRouter(
+            api.routing.websocket_urlpatterns
+        )
     ),
 })
