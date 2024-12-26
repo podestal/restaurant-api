b59fdecc026933380976aae846c15f2c28fcacb1
podestal <luisrodriguezpodesta@gmail.com>
2024-12-26 10:55:19 -0500

    alc settings removed

--- app/restaurant/settings/prod.py
+++ app/restaurant/settings/prod.py
@@ -47,7 +47,3 @@ AWS_DEFAULT_ACL =  None
 AWS_S3_VERIFY = True
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-AWS_S3_OBJECT_PARAMETERS = {
-    'CacheControl': 'max-age=86400',
-    'ACL': 'public-read', 
-}
