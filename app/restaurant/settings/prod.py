54c4474f4baa60c415f00c002d20c9d753745058
podestal <luisrodriguezpodesta@gmail.com>
2024-12-26 11:07:58 -0500

    initial prod setttings

--- app/restaurant/settings/prod.py
+++ app/restaurant/settings/prod.py
@@ -34,9 +34,6 @@ CSRF_TRUSTED_ORIGINS.extend(
 
 USE_X_FORWARDED_HOST = True
 
-MEDIA_URL ='/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
 AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
 AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME")
@@ -46,4 +43,3 @@ AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL =  None
 AWS_S3_VERIFY = True
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-
