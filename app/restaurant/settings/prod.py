b27454e3ecc72dd574bcbffd40600082b1b6e733
podestal <luisrodriguezpodesta@gmail.com>
2024-12-26 09:58:59 -0500

    s3 bucket for media files

--- app/restaurant/settings/prod.py
+++ app/restaurant/settings/prod.py
@@ -24,7 +24,7 @@ CORS_ALLOWED_ORIGINS.extend(
 CORS_ALLOW_CREDENTIALS = True
 
 CORS_ALLOW_HEADERS = list(default_headers) + [
-    'Session-ID',
+    'Session-ID',   
 ]
 
 CSRF_TRUSTED_ORIGINS = []
@@ -32,7 +32,17 @@ CSRF_TRUSTED_ORIGINS.extend(
     filter(None, os.environ.get("DJANGO_CSRF_TRUSTED_ORIGINS", "").split(","))
 )
 
-MEDIA_ROOT = '/vol/media'
-MEDIA_URL = '/media/'
-
 USE_X_FORWARDED_HOST = True
+
+MEDIA_URLS ='/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
+AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME")
+AWS_S3_SIGNATURE_NAME = 's3v4',
+AWS_S3_REGION_NAME = os.environ.get("AWS_S3_REGION_NAME")
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL =  None
+AWS_S3_VERIFY = True
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
